6d5d8df858f49cffb3fd5d7b745d2ad27078bd88
aadi05 <adityabhogate5@gmail.com>
2019-12-29 20:51:23 +0530

    Implementation of Binary search

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,20 @@
-found_at = 0
-
+#Time complexity of search : O(logn)
 def binary_search(arr, item):
     lower = 0
     upper = len(arr)-1
-    
     while lower <= upper:
         mid = (lower+upper)//2
-
         if arr[mid] == item:
-            global found_at
-            found_at = mid
-            return True
+            return mid
         else:
             if arr[mid] > item:
                 upper = mid - 1
             else:
                 lower = mid + 1
+    return None
 
-    return False
-
-
-if binary_search([4,5,7,8,12,34,56,78,89], 12):
-    print(f"Found at index {found_at}")
+found_index = binary_search([4,5,7,8,12,34,56,78,89], 12)
+if found_index:
+    print(f"Found at index {found_index}")
 else:
     print("Not found")
